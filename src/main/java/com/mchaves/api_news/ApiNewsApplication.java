3fb660297df204708f359affb969565d28a66afa
Maurício Chaves <feira.nova@gmail.com>
2024-10-30 19:52:50 -0300

    Atualização

--- src/main/java/com/mchaves/api_news/ApiNewsApplication.java
+++ src/main/java/com/mchaves/api_news/ApiNewsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiParkTwoApplication {
+public class ApiNewsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiParkTwoApplication.class, args);
+		SpringApplication.run(ApiNewsApplication.class, args);
 	}
 
 }
