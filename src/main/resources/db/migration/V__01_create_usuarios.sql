9034fbec75b386c4a9a9b01c019ce4f38faef0c8
mauricio <mauricio.chaves.jr@gmail.com>
2024-12-06 16:42:53 -0300

    develop: Configuração do Flyway

---
+++ src/main/resources/db/migration/V__01_create_usuarios.sql
@@ -0,0 +1,11 @@
+CREATE TABLE usuarios (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(200) NOT NULL,
+    role ENUM('ROLE_ADMIN';'ROLE_USUARIO') NOT NULL,
+    data_criacao DATETIME,
+    data_modificacao DATETIME,
+    criado_por VARCHAR(255),
+    modificado_por VARCHAR(255)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
